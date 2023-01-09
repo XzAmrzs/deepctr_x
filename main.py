877e184e53f44c153bff74d510ad5391e7369c5b
邢志鹏 <jesperxing@kugou.net>
2023-01-09 11:25:44 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from sklearn import datasets
+import numpy as np
+
+x_train = datasets.load_iris().data
+y_train = datasets.load_iris().target
+
+np.random.seed(116)
+np.random.shuffle(x_train)
+np.random.seed(116)
+np.random.shuffle(y_train)
